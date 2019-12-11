f4dc03b62190027fbf4b64de6b128f4ad3681f0f
zcs02468 <zcs02468@163.com>
2019-12-11 20:57:21 +0800

    添加客户端和服务端的通信

--- client/src/page/home/index.vue
+++ client/src/page/home/index.vue
@@ -1,26 +1,47 @@
 <!--  -->
 <template>
     <div>
-        <mavon-editor v-model="value" @save="savePage"/>
+        <el-input v-model="inputValue" placeholder="请输入内容"></el-input>
+        <el-button @click="sendMessage">默认按钮</el-button>
     </div>
 </template>
 
 <script>
+import platform from 'platform';
+import io from 'socket.io-client'
+import settings from './settings.js'
 export default {
     name: "editor",
     data() {
         return {
-            value:''
+            inputValue:'',
+            socket:''
         };
     },
 
     components: {},
 
+    created() {
+        this.socket = io.connect(settings.socket,{'force new connection': true})
+        this.socket.on('open', function() {
+            console.log('已连接')
+        })
+        this.socket.on('news', function (data) {
+            console.log(data);
+            this.socket.emit('my other event', { my: 'data' });
+        });
+    },
+
     methods: {
-        savePage(value) {
-            console.log( value,'value' );
-            console.log( 'this.value', this.value );
+        sendMessage() {
+            console.log( 
+            platform.os.family,
+            platform.name,
+            platform.description, platform,'$$$$$$$');
             
+            this.socket.emit('msg', this.inputValue);
+            this.socket.emit('news', this.inputValue);
+
         }
     },
 
--- server/bin/www.js
+++ server/bin/www.js
@@ -6,9 +6,41 @@ const http = require("http");
 const port = normalizePort(process.env.PORT || "7000");
 const server = http.createServer(app.callback());
 
+
+const io = require('socket.io')(server);
+
 server.listen(port);
 server.on("error", onError);
 server.on("listening", onListening);
+
+
+io.on( 'connection', socket => {
+    
+    socket.emit('open')
+    console.log( '初始化成功' );
+    socket.on('send', data=> {
+        console.log( '客户端发送的消息', data );
+        socket.emit('getMsg', '我是返回的消息... ...');
+    })
+    socket.on('msg',(data)=>{
+        //监听msg事件（这个是自定义的事件）
+        console.log(data);//你好服务器
+        socket.emit('msg','你好浏览器');
+         //向socket用户发送信息
+    })
+    socket.on('news',(data)=>{
+        //监听msg事件（这个是自定义的事件）
+        console.log(data,'eeeeeeee');//你好服务器
+        socket.emit('news','111111111');
+         //向socket用户发送信息
+    })
+    setTimeout( () => {
+        socket.emit('getMsg', '我是初始化3s后的返回消息... ...') 
+    }, 3000)
+} )
+
+
+
 
 function normalizePort(val) {
     let port = parseInt(val, 10);
